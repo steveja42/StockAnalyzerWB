b0ed2f539bc1f15b2d81d4a1ed010ae896f06f94
steveja42 <steveja007@gmail.com>
2016-12-09 19:10:44 -0800

    add a useless comment

--- Sheet1.cs
+++ Sheet1.cs
@@ -7,7 +7,7 @@ using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Extensions.Logging;
 using Microsoft.Office.Interop.Excel;
-
+//a change for newBranch
 namespace StockAnalyzerWB
 {
     public struct Stock
